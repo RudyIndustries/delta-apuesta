13606e034f50ce1d157ed495317ee509ea0fe071
RudyIndustries <rudy.visionary@gmail.com>
2026-06-16 18:40:37 -0400

    Configurar Firebase

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyBuRHz0cNIr1qAQuWnvunJ2BxPmxoxr3OY",
+  authDomain: "delta-apuesta.firebaseapp.com",
+  projectId: "delta-apuesta",
+  storageBucket: "delta-apuesta.firebasestorage.app",
+  messagingSenderId: "711534330448",
+  appId: "1:711534330448:web:9716a3f2dcd22d7e6c8755",
+  measurementId: "G-5HDMNGMZCV",
 };
